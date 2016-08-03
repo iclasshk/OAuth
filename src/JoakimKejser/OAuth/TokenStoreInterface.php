4023462a3803158424f40175606a28eeacd2e09d
Timothy Choi <tyteen4a03@thelastcode.io>
2016-08-03 12:28:28 +0800

    Update docs for TokenStoreInterface.

--- src/JoakimKejser/OAuth/TokenStoreInterface.php
+++ src/JoakimKejser/OAuth/TokenStoreInterface.php
@@ -1,32 +1,51 @@
 <?php
 namespace JoakimKejser\OAuth;
+use JoakimKejser\OAuth\Exception\InvalidConsumerException;
+use JoakimKejser\OAuth\Exception\InvalidTokenException;
 
+/**
+ * An interface for a Token Store.
+ *
+ * @package JoakimKejser\OAuth
+ */
 interface TokenStoreInterface
 {
     /**
-     * @param ConsumerInterface $consumer
-     * @param $tokenType
-     * @param $tokenField
-     * @return mixed
+     * Gets a token.
+     *
+     * @param ConsumerInterface $consumer The consumer this token belongs to.
+     * @param int $tokenType The token type.
+     * @param string $tokenField The token string to look up.
+     * @return TokenInterface
+     * @throws InvalidConsumerException
+     * @throws InvalidTokenException
+     * @see TokenType
      */
     public function getToken(ConsumerInterface $consumer, $tokenType, $tokenField);
 
     /**
-     * @param ConsumerInterface $consumer
-     * @param null $callback
+     * Creates a new request token.
+     *
+     * @param ConsumerInterface $consumer The consumer.
+     * @param array|string $callback All valid callback URLs.
      * @return TokenInterface
+     * @throws InvalidConsumerException
      */
     public function newRequestToken(ConsumerInterface $consumer, $callback = null);
 
     /**
-     * @param TokenInterface $requestToken
-     * @param ConsumerInterface $consumer
-     * @return TokenInterface access token
+     * Creates a new access token.
+     *
+     * @param TokenInterface $requestToken The request token related to this request.
+     * @param ConsumerInterface $consumer The consumer.
+     * @return TokenInterface
+     * @throws InvalidConsumerException
+     * @throws InvalidTokenException If the request token is invalid.
      */
     public function newAccessToken(TokenInterface $requestToken, ConsumerInterface $consumer);
 
     /**
-     * Removes a request token.
+     * Removes a request token from the store.
      *
      * @param TokenInterface $requestToken The request token to remove.
      * @return void
@@ -34,7 +53,7 @@ interface TokenStoreInterface
     public function removeRequestToken(TokenInterface $requestToken);
 
     /**
-     * Removes an access token.
+     * Removes an access token from the store.
      *
      * @param TokenInterface $accessToken The access token to remove.
      * @return void
